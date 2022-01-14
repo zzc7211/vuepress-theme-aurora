3b0b4ca126c4498e813adf3f0f51f23b91164ab2
zhouzichun <zhouzichun@xiaomi.com>
2022-01-14 13:20:52 +0800

    [build]

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ const { path } = require("@vuepress/utils");
 module.exports = {
     //base: '/REPO/',//如果你准备发布到 https://<USERNAME>.github.io/<REPO>/ ，也就是说你的仓库地址是 https://github.com/<USERNAME>/<REPO> ，则将 base 设置为 "/<REPO>/"。
     //https://ooszy.cco.vin/img/blog-note/aurora-loading.gif
+    base: '/blog/' ,
     extendsMarkdown: (md) => {
 
     },
--- readme.md
+++ readme.md
@@ -116,3 +116,10 @@ aurora demo-blog
 ![](https://ooszy.cco.vin/img/theme/phoone.jpg)
 ![](https://ooszy.cco.vin/img/theme/photo-theme.jpg)
 
+```
+# 建议不要直接使用 cnpm 安装依赖，会有各种诡异的 bug。可以通过如下操作解决 npm 下载速度慢的问题
+npm install --registry=https://registry.npm.taobao.org
+
+# 启动服务
+npm run dev
+```
